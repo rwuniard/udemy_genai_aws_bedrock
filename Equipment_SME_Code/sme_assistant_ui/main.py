653505ba95796d633328b49391bbf17792154cbe
rwuniard <rwuniard@hotmail.com>
2026-04-23 20:02:50 -0400

    Sending prompt to the API gateway then display the result.

--- Equipment_SME_Code/sme_assistant_ui/main.py
+++ Equipment_SME_Code/sme_assistant_ui/main.py
@@ -1,10 +1,26 @@
 import streamlit as st
+import requests
+import json
 
 def main():
     print("Hello from sme-assistant-ui!")
     prompt = st.chat_input("Enter your prompt here")
     if prompt:
-        st.write(f"You entered: {prompt}")
+        with st.chat_message("user"):
+            st.write(prompt)
+            response = requests.post(
+                "https://v749g1yxl6.execute-api.us-east-1.amazonaws.com/test/sme_assistant",
+                json={"prompt": prompt}
+            )
+            if response.status_code == 200:
+                response_data = response.json()
+                if isinstance(response_data, dict) and "body" in response_data:
+                    result = json.loads(response_data["body"])
+
+        with st.chat_message("assistant"):
+           st.write(result)
+
+   
 
 if __name__ == "__main__":
     main()
