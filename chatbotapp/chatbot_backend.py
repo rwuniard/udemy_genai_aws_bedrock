4f74b96e3047260a870b969b109ffa6176b6bd10
rwuniard <rwuniard@hotmail.com>
2026-05-01 14:59:48 -0400

    Refactor chatbot backend to use a class-based structure, implementing an agent for message handling. Update dependencies to include 'rich' for improved output formatting.

--- chatbotapp/chatbot_backend.py
+++ chatbotapp/chatbot_backend.py
@@ -1,23 +1,58 @@
 from langchain_aws import ChatBedrockConverse
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
+from rich.pretty import pprint
 
 
-def get_chatbot():
-    return ChatBedrockConverse(
-        credentials_profile_name="default",
-        model="us.deepseek.r1-v1:0",
-        temperature=0.7,
-        max_tokens=1000
-    )
+class ChatbotBackend:
+    def __init__(self):
+        self.agent = self.get_agent()
 
+    def send_message(self, message):
+        messages = [
+            {"role": "user", "content": message}
+        ]
+        return self.agent.invoke(messages)
+
+    def get_llm(self):
+        return ChatBedrockConverse(
+            credentials_profile_name="default",
+            model="us.deepseek.r1-v1:0",
+            temperature=0.7,
+            max_tokens=1000
+        )
+
+    def get_agent(self):
+        model = self.get_llm()
+        return create_agent(
+            model=model,
+            tools=[], 
+            checkpointer=InMemorySaver()
+        )
+
+    def send_message(self, message):
+        messages = [
+            {"role": "user", "content": message}
+        ]
+        result = self.agent.invoke(
+            {
+                "messages": messages,
+                "user_id": "user_123",
+                "preferences": {"theme": "dark"}
+            },
+            {"configurable": {"thread_id": "1"}}
+        )
+        return result["messages"][-1].content[0]["text"]
 
 def main():
     print("Hello from chatbot_backend!")
-    messages = [
-        {"role": "user", "content": "What is the capital of France?"}
-    ]
-    chatbot = get_chatbot()
-    response = chatbot.invoke(messages)
-    print(response)
+    chatbot = ChatbotBackend()
+    user_input = ""
+    while user_input != "exit":
+        user_input = input("Enter your message: ")
+        response = chatbot.send_message(user_input)
+        print("Response:", response)
+
 
 if __name__ == "__main__":
     main()
